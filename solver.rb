3d9694cf776bb8cb0d082c7f6a830a77d9d62e55
dazehinn <dazehinn@yahoo.fr>
2023-07-06 17:14:38 +0100

    add factorial and fizzbuzz methods

--- solver.rb
+++ solver.rb
@@ -1,11 +1,25 @@
 class Solver
-  def factorial
+  def factorial(num)
+    if num.zero?
+      1
+    else
+      num * factorial(num - 1)
+    end
   end
 
   def reverse(word)
     word.reverse
   end
 
-  def fizzbuzz
+  def fizzbuzz(num)
+    if (num % 5).zero? && (num % 3).zero?
+      'fizzbuzz'
+    elsif (num % 5).zero?
+      'buzz'
+    elsif (n % 3).zero?
+      'fizz'
+    else
+      num.to_s
+    end
   end
 end
